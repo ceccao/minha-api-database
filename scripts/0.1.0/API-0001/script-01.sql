64acd0a814bcad1ad4b1372eb699a36dfbf33a92
Gabriel Ceccon <gabrielceccon3@gmail.com>
2026-09-09 23:17:49 -0300

    fix: ajusta tabela

--- scripts/0.1.0/API-0001/script-01.sql
+++ scripts/0.1.0/API-0001/script-01.sql
@@ -1,11 +1,8 @@
 -- API-0001: Criação da tabela produto
-CREATE TABLE produto (
-    id INT NOT NULL AUTO_INCREMENT COMMENT 'Chave primária da tabela',
-    nome VARCHAR(100) NOT NULL COMMENT 'Nome do produto',
-    preco DECIMAL(18,2) NOT NULL COMMENT 'Preço do produto',
-    ativo BOOLEAN NOT NULL DEFAULT TRUE COMMENT 'Status do produto (soft delete: 1 ativo, 0 inativo)',
-    criado_em DATETIME(6) NOT NULL COMMENT 'Data/hora de criação, sempre em UTC',
-    atualizado_em DATETIME(6) NULL COMMENT 'Data/hora da última atualização, sempre em UTC',
-    version INT NOT NULL DEFAULT 0 COMMENT 'Controle de concorrência otimista (NHibernate)',
-    PRIMARY KEY (id)
+CREATE TABLE PRODUTO (
+    ID INT NOT NULL AUTO_INCREMENT COMMENT 'Chave primária da tabela',
+    NOME VARCHAR(100) NOT NULL COMMENT 'Nome do produto',
+    PRECO DECIMAL(18,2) NOT NULL COMMENT 'Preço do produto',
+    SITUACAO INT NOT NULL COMMENT 'Status do produto (soft delete: 1 ativo, 0 inativo)',
+    PRIMARY KEY (ID)
 ) COMMENT = 'Armazena os produtos cadastrados';
